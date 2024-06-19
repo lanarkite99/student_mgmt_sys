9c937fb44a5c655922bd891e0aff1964ee5f86f6
Meet <meetapple191@gmail.com>
2024-06-19 14:48:04 +0530

    Initial Commit:Setting up GUI

---
+++ main.py
@@ -0,0 +1,32 @@
+from PyQt6.QtWidgets import (QLabel,QApplication,QWidget,QGridLayout,QLineEdit,
+                             QPushButton,QMainWindow,QTableWidget)
+from PyQt6.QtGui import QAction
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+
+        file_menu_item=self.menuBar().addMenu("&File")
+        help_menu_item=self.menuBar().addMenu("&Help")
+
+        add_student_action=QAction("Add Student",self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action=QAction("About",self)
+        help_menu_item.addAction(about_action)
+
+        self.table=QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id","Name","Course","Mobile"))
+        self.setCentralWidget(self.table)
+    def load_data(self):
+        pass
+
+
+app=QApplication(sys.argv)
+mainwindow=MainWindow()
+mainwindow.show()
+sys.exit(app.exec())
+
